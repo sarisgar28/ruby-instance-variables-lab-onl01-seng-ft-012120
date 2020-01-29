02c55e6c8db065eb44a592e88c65830f3adb4876
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-29 22:28:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+Class Dog 
+def name= 
+  @this_dogs_name
+  end
+end 
+fido = dog.new 
+  fido.name = "fido"
+    puts fido.name 
+  end 
+  
+  def name 
+    @this_dogs_name
+    lassie = dog.name 
+    lassie.name = "lassie"
+   puts lassie.name 
+  end
+end
